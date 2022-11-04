1899fca9ed4b364c18a1bcbbbafa2db4b4e1cb49
Babatunde Owoleke <tundeowoleke1@gmail.com>
2022-11-04 21:17:54 +0100

    Utils function for Implicit Intent

---
+++ app/src/main/java/com/t2dbabz/mobileresume/ui/util/Utils.kt
@@ -0,0 +1,29 @@
+package com.t2dbabz.mobileresume.ui.util
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+fun Context.sendMail(to: String, subject: String) {
+    try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "vnd.android.cursor.item/email" // or "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(to))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+
+    } catch (t: Throwable) {
+
+    }
+}
+
+fun Context.dial(phone: String) {
+    try {
+        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+        startActivity(intent)
+    } catch (t: Throwable) {
+
+    }
+}
