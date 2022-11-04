54a81bc80de65bfa52d71bbfc81f31023a6c2936
Babatunde Owoleke <tundeowoleke1@gmail.com>
2022-11-04 20:38:08 +0100

    Implement WorkExperience Screen

--- app/src/main/java/com/t2dbabz/mobileresume/ui/screens/work_experience/WorkExperienceScreen.kt
+++ app/src/main/java/com/t2dbabz/mobileresume/ui/screens/work_experience/WorkExperienceScreen.kt
@@ -1,16 +1,27 @@
 package com.t2dbabz.mobileresume.ui.screens.work_experience
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.t2dbabz.mobileresume.ui.screens.work_experience.component.WorkExperienceItem
 
 @Destination
 @Composable
-fun WorkExperience() {
+fun WorkExperience(viewModel: WorkExperienceViewModel = viewModel()) {
+    val state by viewModel.state
     Box(modifier = Modifier.fillMaxSize()){
-        Text(text = "Work Experience Screen")
+        LazyColumn(contentPadding = PaddingValues(8.dp)) {
+            items(state.workExperiences){
+                WorkExperienceItem(workExperience = it)
+            }
+        }
     }
 }
